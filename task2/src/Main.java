4a1ca3d5b10da74ac5fbbd419a51eacd26187c62
PowerlineR413 <Powerline413@yandex.ru>
2024-08-05 17:36:47 +0300

    Решение набора задач 2

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -1,34 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\t\t\t***Программа определения весов товаров***\n\n\n");
+        System.out.println("\t\t\t***Программа определения веса товаров***\n");
 
         System.out.println("Введите первый вес: ");
         double first = new Scanner(System.in).nextDouble();
-
         System.out.println("Введите второй вес: ");
         double second = new Scanner(System.in).nextDouble();
-
         System.out.println("Введите третий вес: ");
         double third = new Scanner(System.in).nextDouble();
 
-        double greatest = 0;
-        double average = 0;
-        double smallest = 0;
         //todo дописать логику программы ниже.
 
-
-
-
-
-
-
+        double[] testArray = new double[]{first,second,third};
+        Arrays.sort(testArray);
+        double greatest = testArray[2];
+        double average = testArray[1];
+        double smallest = testArray[0];
         //todo
-
-
-        System.out.println("Наибольший вес: " + 10);
-        System.out.println("Средний вес: " + 9);
-        System.out.println("Наименьший вес: " + 1);
+        System.out.println("Наибольший вес: " + greatest);
+        System.out.println("Средний вес: " + average);
+        System.out.println("Наименьший вес: " + smallest);
     }
 }
+
